e7d47889fdc74d95fe757b97c798b6df22e1eb9c
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2025-11-08 17:53:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Asaaq-ali/vcbnm",
+    "https://github.com/Teemsoha/vcbnm",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
